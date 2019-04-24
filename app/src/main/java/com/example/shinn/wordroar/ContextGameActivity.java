fc0297e55bcaea7ef5bebec7f72a230f456e5bf4
Sinead Hegarty <shin.nai.nai@gmail.com>
2019-04-24 12:24:06 +0100

    Show success or failure for answer

--- app/src/main/java/com/example/shinn/wordroar/ContextGameActivity.java
+++ app/src/main/java/com/example/shinn/wordroar/ContextGameActivity.java
@@ -1,9 +1,11 @@
 package com.example.shinn.wordroar;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 public class ContextGameActivity extends AppCompatActivity {
 
@@ -14,8 +16,29 @@ public class ContextGameActivity extends AppCompatActivity {
 
         ImageButton homeBtn = findViewById(R.id.home_button);
         homeBtn.setOnClickListener((view) -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            finish();
         });
+
+        Button submitBtn = findViewById(R.id.context_game_submit_button);
+        RadioGroup group = findViewById(R.id.answers_group);
+        submitBtn.setOnClickListener((view) -> {
+            int id = group.getCheckedRadioButtonId();
+            if(id == 2) {
+                showSuccessPage();
+            } else
+                showFailurePage();
+        });
+
+    }
+
+    private void showSuccessPage() {
+        Intent intent = new Intent(this, SuccessActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showFailurePage() {
+        Intent intent = new Intent(this, FailureActivity.class);
+        startActivity(intent);
     }
 }
