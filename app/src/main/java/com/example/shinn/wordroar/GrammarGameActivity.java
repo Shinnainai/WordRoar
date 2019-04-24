227cef4a95ffdac6d1fc7fe09e18779d7be007a1
Sinead Hegarty <shin.nai.nai@gmail.com>
2019-04-24 11:55:20 +0100

    If answer is correct display success activity

--- app/src/main/java/com/example/shinn/wordroar/GrammarGameActivity.java
+++ app/src/main/java/com/example/shinn/wordroar/GrammarGameActivity.java
@@ -1,8 +1,10 @@
 package com.example.shinn.wordroar;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class GrammarGameActivity extends AppCompatActivity {
@@ -16,6 +18,20 @@ public class GrammarGameActivity extends AppCompatActivity {
         homeBtn.setOnClickListener((view) -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+            finish();
         });
+
+        Button submitBtn = findViewById(R.id.grammar_game_answer_button);
+        submitBtn.setOnClickListener((view) -> {
+            EditText editText = findViewById(R.id.answer_box_edittext);
+            if(editText.getText().toString().equalsIgnoreCase("Do you think it will rain today?")) {
+                displaySuccessPage();
+            }
+        });
+    }
+
+    private void displaySuccessPage() {
+        Intent intent = new Intent(this, SuccessActivity.class);
+        startActivity(intent);
     }
 }
