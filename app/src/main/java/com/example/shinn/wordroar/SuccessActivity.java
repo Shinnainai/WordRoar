831ddaeacb5b393a3e20e074b9874eaf8bb1516d
Sinead Hegarty <shin.nai.nai@gmail.com>
2019-05-06 17:42:48 +0100

    Added Add Score to Success Activity

--- app/src/main/java/com/example/shinn/wordroar/SuccessActivity.java
+++ app/src/main/java/com/example/shinn/wordroar/SuccessActivity.java
@@ -1,7 +1,9 @@
 package com.example.shinn.wordroar;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        SharedPreferences sharedPref = this.getPreferences(MODE_PRIVATE);
+        int currentHighScore = sharedPref.getInt(getString(R.string.saved_high_score_key), 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        int newHighScore = currentHighScore + 100;
+        editor.putInt(getString(R.string.saved_high_score_key), newHighScore);
+        editor.apply();
+
+        TextView total = findViewById(R.id.success_total);
+        total.setText("You now have " + newHighScore + " points");
     }
 }
